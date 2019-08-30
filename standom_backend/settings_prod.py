4d0d0402f04ce4bb7af3cbf12fea306ac2f3d04f
root <root@sta.standomsports.com>
2019-08-30 01:50:21 +0000

    update setting of prod

--- standom_backend/settings_prod.py
+++ standom_backend/settings_prod.py
@@ -1,8 +1,8 @@
-DEBUG = False
-ALLOWED_HOSTS = ['192.185.71.147', 'standomsports.com']
+#DEBUG = False
+ALLOWED_HOSTS = ['192.232.213.37', 'standomsports.com']
 STATIC_URL = '/static/'
-STATIC_ROOT = '/var/www/html/standom/'
-MEDIA_ROOT = '/var/www/html/standom/'
+STATIC_ROOT = '/var/www/html/standom/static'
+MEDIA_ROOT = '/var/www/html/standom/upload'
 MEDIA_URL = '/media/'
 
 print("Debug Flase")
